ae5538ffcaee0ee6d67b3c58b9143fb4ac97a1cd
Sarah Conway <sarahconway@Sarahs-MBP.fc.ca>
2020-01-08 10:36:37 -0500

    vue demo

---
+++ js/main.js
@@ -0,0 +1,26 @@
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
+
+// set up a viewmodel and use the Vue Framework
+
+const my_vm = (() => {
+    // variables , or define components... whatever
+
+    let vue_VM = new Vue({
+        data: {
+            message: "Hello from Vue!",
+
+            collection: [
+                {name: "Joe", role: "Prof"},
+                {name: "John", role: "Prof"},
+                {name: "Jarrod", role: "Prof"}
+            ]
+        },
+
+        methods: {
+            logClicked() {
+                console.log('clicked on an element');
+            }
+        }
+    }).$mount("#app");
+
+})();
